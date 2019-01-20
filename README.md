46c6cabb633731c1f8b88341061cffa27ba334e3
Nick Socha <38092580+nicksocha@users.noreply.github.com>
2019-01-19 22:31:00 -0600

    Initial Upload 01192019

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-nicksocha-net
+# nicksocha.net
+
+NickSocha.net is a simple website I threw together to practice CSS Grid. It has a few links on it that I always need out in the wild but always forget the URLs.
+
+Check it out over at [NickSocha.net](https://nicksocha.net/).
---
+++ js/app.js
@@ -0,0 +1,13 @@
+const modal = document.getElementById("myModal");
+
+const btn = document.getElementById("myBtn");
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
