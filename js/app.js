1be60a1b41d0ae625fb3a807d26ffbd691fc136d
Nick Socha <38092580+nicksocha@users.noreply.github.com>
2019-01-20 00:04:19 -0600

    Added update footer year automatically

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
+// Modal
 const modal = document.getElementById("myModal");
-
 const btn = document.getElementById("myBtn");
 
-btn.onclick = function () {
+btn.onclick = function() {
   modal.style.display = "block";
 };
 
-window.onclick = function (event) {
+window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
+
+// Automatically update year in footer
+
+const yyyy = new Date().getFullYear();
+document.getElementById("year").innerHTML = `Nick Socha dot net #${yyyy}`;
