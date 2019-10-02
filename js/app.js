9c504035bf0b3b0b556e092dc98d1fdbbe9b93e3
Nick Socha <38092580+nicksocha@users.noreply.github.com>
2019-10-01 21:05:09 -0500

    Removed iframe from contact form

--- js/app.js
+++ js/app.js
@@ -1,18 +1,4 @@
-// Modal
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
-
-btn.onclick = function() {
-  modal.style.display = "block";
-};
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
-
 // Automatically update year in footer
 
 const yyyy = new Date().getFullYear();
-document.getElementById("year").innerHTML = `Nick Socha dot net #${yyyy}`;
+document.getElementById('year').innerHTML = `Nick Socha dot net #${yyyy}`;
